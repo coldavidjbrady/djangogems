8335d28767bd27e9739f615f1cc5a5e95cf7d4b2
davidbrady <coldavidjbrady@gmail.com>
2016-02-16 18:28:10 -0800

    Updated serializers.py so a complete JSON object is returned with a product get request.

--- store/serializers.py
+++ store/serializers.py
@@ -3,32 +3,45 @@ from rest_framework import serializers
 from rest_framework.reverse import reverse
 from .models import *
 
+class ImageSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Image
+        fields = ('path', )
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+
+    links = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Review
+        fields = ('stars', 'body', 'author', 'createdOn', 'product', 'links')
+
+    def get_links(self, obj):
+        request = self.context['request']
+        links = {
+            'self': reverse('review-detail', kwargs={'pk':obj.pk}, request=request),
+
+        }
+        return links
+
+
 class ProductSerializer(serializers.ModelSerializer):
 
     links = serializers.SerializerMethodField()
-    reviews = serializers.StringRelatedField(many=True)
-    images = serializers.StringRelatedField(many=True)
 
-    '''
-    # The following are useful for returning links versus string representations
-    reviews = serializers.HyperlinkedRelatedField(
+    reviews = ReviewSerializer(many=True, read_only=True)
+    images = serializers.SlugRelatedField(
         many=True,
         read_only=True,
-        view_name='review-detail'
-    )
-
-    images = serializers.HyperlinkedRelatedField(
-        many=True,
-        read_only=True,
-        view_name='image-detail'
-    )
-    '''
+        slug_field='path'
+     )
 
     class Meta:
       model = Product
-      fields = ('id', 'name', 'description', 'shine', 'price', 'rarity', 'color',
-                  'faces', 'links', 'reviews', 'images')
-
+      fields = ('id', 'name','shine', 'price', 'rarity', 'color',
+                  'faces', 'links','reviews', 'images')
 
     def get_links(self, obj):
         request = self.context['request']
@@ -40,21 +53,8 @@ class ProductSerializer(serializers.ModelSerializer):
 
 
 
-class ImageSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Image
-        fields = ('path','product')
 
 
-class ReviewSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Review
-        fields = ('stars', 'body', 'author', 'createdOn', 'product', )
-
-    def __str__(self):
-        return '%d: %s' % self.id, self.product.name
 
 
 
