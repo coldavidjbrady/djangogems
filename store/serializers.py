da4f8bed95f577837d6fc9533271f9da836fca13
davidjbrady <coldavidjbrady@gmail.com>
2016-03-03 16:20:21 -0800

    Updated serializers.py so reviews can be deleted from Django REST Framework

--- store/serializers.py
+++ store/serializers.py
@@ -3,58 +3,62 @@ from rest_framework import serializers
 from rest_framework.reverse import reverse
 from .models import *
 
-class ImageSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Image
-        fields = ('path', )
-
-
 class ReviewSerializer(serializers.ModelSerializer):
 
-    links = serializers.SerializerMethodField()
+    revlinks = serializers.SerializerMethodField()
 
     class Meta:
         model = Review
-        fields = ('stars', 'body', 'author', 'createdOn', 'product', 'links')
+        fields = ('stars', 'body', 'author', 'createdOn', 'product', 'revlinks')
 
-    def get_links(self, obj):
+    def get_revlinks(self, obj):
         request = self.context['request']
-        links = {
+        revlinks = {
             'self': reverse('review-detail', kwargs={'pk':obj.pk}, request=request),
-
         }
-        return links
+        return revlinks
 
+    def __str__(self):
+        return '%d: %s' % self.id, self.product.name
 
 class ProductSerializer(serializers.ModelSerializer):
 
     links = serializers.SerializerMethodField()
-
-    reviews = ReviewSerializer(many=True, read_only=True)
+    reviews = ReviewSerializer(many=True)
     images = serializers.SlugRelatedField(
         many=True,
         read_only=True,
         slug_field='path'
      )
 
+    '''
+    # The following are useful for returning links versus string representations
+    reviews = serializers.HyperlinkedRelatedField(
+        many=True,
+        read_only=True,
+        view_name='review-detail'
+    )
+    '''
+
     class Meta:
       model = Product
-      fields = ('id', 'name','shine', 'price', 'rarity', 'color',
-                  'faces', 'links','reviews', 'images')
+      fields = ('id', 'name', 'description', 'shine', 'price', 'rarity', 'color',
+                  'faces', 'links', 'reviews', 'images')
+
 
     def get_links(self, obj):
         request = self.context['request']
         links = {
             'self': reverse('product-detail', kwargs={'pk':obj.pk}, request=request),
-
         }
         return links
 
 
 
+class ImageSerializer(serializers.ModelSerializer):
 
-
-
+    class Meta:
+        model = Image
+        fields = ('path', 'product')
 
 
